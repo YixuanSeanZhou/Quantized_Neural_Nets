6414b2beae168205f60ba284c1b4c9bede6653e9
Jinjie Zhang <jayzhang0727@gmail.com>
2021-10-26 11:02:46 -0700

    Run

--- quantization_scripts/quantize.py
+++ quantization_scripts/quantize.py
@@ -11,14 +11,14 @@ if __name__ == '__main__':
     batch_size = 32  # batch_size used for quantization
     num_workers = 4
     # load the model to be quantized
-    model = torch.load('../models/mnist_mlp.pt', map_location=torch.device('cpu'))
+    model = torch.load('../models/fashion_mlp.pt', map_location=torch.device('cpu'))
 
     # load the data loader for training and testing
-    train_loader, _, test_loader = load_data_mnist(batch_size, train_ratio=1, 
+    train_loader, _, test_loader = load_data_fashion_mnist(batch_size, train_ratio=1, 
                                                 num_workers=num_workers)
 
     # quantize the neural net
-    quantizer = QuantizeNeuralNet(model, batch_size, train_loader, bits=2)
+    quantizer = QuantizeNeuralNet(model, batch_size, train_loader, bits=3)
     quantized_mlp = quantizer.quantize_network()
     predictions, labels = test_mlp(test_loader, quantized_mlp)
     test_accuracy = np.sum(predictions == labels) / len(labels)
