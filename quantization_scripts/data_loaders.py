7e4ae62d6d653f2f18ab6b5d028da0d44640d591
Jinjie Zhang <jayzhang0727@gmail.com>
2021-11-02 16:48:26 -0700

    Added seed_worker to guarantee reproducibility

--- quantization_scripts/data_loaders.py
+++ quantization_scripts/data_loaders.py
@@ -2,7 +2,18 @@ import torch
 import torchvision
 from torch.utils.data import Dataset, DataLoader, random_split
 from torchvision import transforms
+import numpy as np
+import random
 import multiprocessing as mp
+
+def seed_worker(worker_id):
+    worker_seed = torch.initial_seed() % 2**32
+    np.random.seed(worker_seed)
+    random.seed(worker_seed)
+
+g = torch.Generator()
+g.manual_seed(0)
+# use above function and g to preserve reproducibility.
 
 def get_dataloader_workers():
     return mp.cpu_count() - 1
@@ -25,10 +36,10 @@ def load_data_fashion_mnist(batch_size, resize=None, train_ratio=0.8, num_worker
     train_size = int(len(mnist_train) * train_ratio)
     test_size =  len(mnist_train) - train_size
     # Split dataset and the generator is used for reproducible results:    
-    train_data, val_data = random_split(mnist_train, [train_size, test_size], 
-                                generator=torch.Generator().manual_seed(42))   
-    train_loader = DataLoader(train_data, batch_size, shuffle=True,
-                        num_workers=num_workers)
+    train_data, val_data = random_split(mnist_train, [train_size, test_size], generator=g)
+                                 
+    train_loader = DataLoader(train_data, batch_size, shuffle=True, num_workers=num_workers,
+                            worker_init_fn=seed_worker, generator=g)
     val_loader =  DataLoader(val_data, batch_size, shuffle=False,
                         num_workers=num_workers)
     test_loader = DataLoader(mnist_test, batch_size=1, shuffle=False,
@@ -53,10 +64,9 @@ def load_data_mnist(batch_size, resize=None, train_ratio=0.8, num_workers=get_da
     train_size = int(len(mnist_train) * train_ratio)
     test_size =  len(mnist_train) - train_size
     # Split dataset and the generator is used for reproducible results:    
-    train_data, val_data = random_split(mnist_train, [train_size, test_size], 
-                                generator=torch.Generator().manual_seed(42))   
-    train_loader = DataLoader(train_data, batch_size, shuffle=True,
-                        num_workers=num_workers)
+    train_data, val_data = random_split(mnist_train, [train_size, test_size], generator=g)   
+    train_loader = DataLoader(train_data, batch_size, shuffle=True, num_workers=num_workers, 
+                            worker_init_fn=seed_worker, generator=g)
     val_loader =  DataLoader(val_data, batch_size, shuffle=False,
                         num_workers=num_workers)
     test_loader = DataLoader(mnist_test, batch_size=1, shuffle=False,
@@ -81,10 +91,10 @@ def load_data_kmnist(batch_size, resize=None, train_ratio=0.8, num_workers=get_d
     train_size = int(len(mnist_train) * train_ratio)
     test_size =  len(mnist_train) - train_size
     # Split dataset and the generator is used for reproducible results:    
-    train_data, val_data = random_split(mnist_train, [train_size, test_size], 
-                                generator=torch.Generator().manual_seed(42))   
+    train_data, val_data = random_split(mnist_train, [train_size, test_size], generator=g)
+                                 
     train_loader = DataLoader(train_data, batch_size, shuffle=True,
-                        num_workers=num_workers)
+                        num_workers=num_workers, worker_init_fn=seed_worker, generator=g)
     val_loader =  DataLoader(val_data, batch_size, shuffle=False,
                         num_workers=num_workers)
     test_loader = DataLoader(mnist_test, batch_size=1, shuffle=False,
