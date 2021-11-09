63b698e61876808eaac9145e46d13d5d88f3459f
Jinjie Zhang <jayzhang0727@gmail.com>
2021-11-09 11:26:53 -0800

    Modified

--- logs/init_log.py
+++ logs/init_log.py
@@ -2,14 +2,12 @@ import csv
 
 log_file_name = 'Quantization_Log.csv'
 
-fields = ['Model Name', 'Dataset', 'Quantization Batch Size', 'Original Test Accuracy',
-          'Quantized Top1 Accuracy', 'Quantized Top5 Accuracy', 'Bits', 'Alphabet_Scalar', 
+fields = ['Model Name', 'Dataset', 'Quantization Batch Size', 'Original Top1 Accuracy',
+          'Quantized Top1 Accuracy', 'Original Top5 Accuracy', 'Quantized Top5 Accuracy', 'Bits', 'Alphabet_Scalar', 
           'Include 0', 'Seed', 'Author']
 
 if __name__ == '__main__':
-    pass
+    with open(log_file_name, 'w') as f:
+        writer = csv.DictWriter(f, fieldnames=fields)
 
-with open(log_file_name, 'w') as f:
-    writer = csv.DictWriter(f, fieldnames=fields)
-
-    writer.writeheader()
+        writer.writeheader()
